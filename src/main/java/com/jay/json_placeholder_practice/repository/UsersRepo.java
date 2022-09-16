b0c8d26bc17e734cfa9084f4cfe52502371a7b22
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-16 06:53:39 -0400

    extends the interface and use the JpaRepository to perform CRUD operations on a database

--- src/main/java/com/jay/json_placeholder_practice/repository/UsersRepo.java
+++ src/main/java/com/jay/json_placeholder_practice/repository/UsersRepo.java
@@ -1,4 +1,16 @@
 package com.jay.json_placeholder_practice.repository;
 
-public interface UsersRepo {
+import com.jay.json_placeholder_practice.model.UsersModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public is an access modifier which defines who can access this interface.
+//interface is a keyword which defines an interface in java.
+//UsersRepo id the name of the interface.
+//extends are a keyword which is used to extend a class or interface
+//JpaRepository is a class which is used to perform CRUD operations on a database.
+//UserModel is a class which is used to define the structure of the table.
+//Long is a data type which defines that this interface can only store numbers.
+public interface UsersRepo extends JpaRepository<UsersModel, Long> {
+
 }
+
