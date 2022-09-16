bd830eb514bf60579d7f15a9af151dbdff83abb1
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-16 06:00:51 -0400

    Initial commit

---
+++ src/test/java/com/jay/json_placeholder_practice/JsonPlaceholderPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.json_placeholder_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceholderPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
