6791fb542325f994cf7ff46f0b833d655da9fbde
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-16 21:58:11 -0400

    Create a PostMapping to create a neu user and save the user record in a database

--- src/main/java/com/jay/json_placeholder_practice/controller/UsersController.java
+++ src/main/java/com/jay/json_placeholder_practice/controller/UsersController.java
@@ -3,10 +3,9 @@ package com.jay.json_placeholder_practice.controller;
 import com.jay.json_placeholder_practice.model.UsersModel;
 import com.jay.json_placeholder_practice.repository.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 //@ResController is an annotation which is used to tell spring that this class is a controller.
@@ -70,6 +69,28 @@ public class UsersController {
         //usersRepo is the name of the field.
         //findAll is a method which is used to get all the records from a database.
         return ResponseEntity.ok(usersRepo.findAll());
+    }
+
+    //@PostMapping is an annotation which is used to tell spring that this method is a post mapping. It is used to create a record in a database table. It is used to create a new user. It is used to create a new post.
+    //("/create) is a request mapping which is used to map the url to the controller class.
+    @PostMapping("/create")
+    //public is an access modifier which defines who can access this method.
+    //ResponseEntity is a class which is used to send a response to the client with a status code.
+    //<UsersModel> is a wildcard which defines that this method can return any type of data.
+    //createUsers is the name of the method.
+    //@RequestBody is an annotation which is used to tell spring that this parameter is a request body. It is used to get the data from the request body.
+    //UsersModel is a class which is used to define the structure of the table.
+    //newUsersModel is the name of the parameter.
+    public ResponseEntity<UsersModel> createUsers (@RequestBody UsersModel newUsersModel) {
+        //return is a keyword which is used to return a value from a method.
+        //new is a keyword which is used to create a new object.
+        //ResponseEntity is a class which is used to send a response to the client with a status code.
+        //usersRepo is the name of the field.
+        //save is a method which is used to save a record in a database.
+        //newUsersModel is the name of the parameter.
+        //HttpStatus is an enum which is used to define the status code of the response.
+        //CREATED is a constant which is used to define the status code of the response.
+        return new ResponseEntity<>(usersRepo.save(newUsersModel), HttpStatus.CREATED);
     }
 
 
